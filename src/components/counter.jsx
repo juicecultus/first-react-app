efc90ced4d25b7295e63efebc498ab029ec0326d
J B <justinian.boghici@gmail.com>
2020-01-28 21:47:23 +0000

    Bind Event Handlers - option 2 - arrow function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,14 @@ class Counter extends Component {
     tags: []
   };
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this); // returns a new function
-  }
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
 
-  handleIncrement() {
+  // convert function into arrow function (arrow functions do not rebind 'this' keyword - they inherit it)
+  handleIncrement = () => {
     console.log('Increment Clicked', this);
-  }
+  };
 
   render() {
     return (
