4a00490c005998daaeb908162545164e2d4b4ba7
J B <justinian.boghici@gmail.com>
2020-01-28 12:56:18 +0000

    Setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,16 @@ class Counter extends Component {
     count: 0
   };
 
+  styles = {
+    fontSize: 50,
+    fontWeight: 'bold'
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
       </React.Fragment>
     );
   }
