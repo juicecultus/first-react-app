ff7832e1d0dca0b79902fd2739e306cd4c2e50cf
J B <justinian.boghici@gmail.com>
2020-01-29 22:22:49 +0000

    update the State of component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import Counters from './counters';
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    count: this.props.counter.value
   };
 
   handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className='btn btn-secondary btn-sm'
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className='btn btn-danger btn-sm m-2'
+        >
+          Delete
         </button>
       </div>
     );
