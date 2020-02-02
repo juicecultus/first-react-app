95fcf54743f7c2752742f5e56c62a3b88552e3e0
J B <justinian.boghici@gmail.com>
2020-02-02 13:32:19 +0000

    add decrement button and functionality

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,7 +5,13 @@ class Counters extends Component {
   render() {
     console.log('Counters - Rendered');
 
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onIncrement,
+      onDecrement
+    } = this.props;
 
     return (
       <div>
@@ -17,6 +23,7 @@ class Counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
